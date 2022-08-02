9c9013ddbfd35b508f49382d2dd143cd0b3c1b50
Avrigeanu Sebastian <avrigeanusebastian@gmail.com>
2022-08-02 09:08:49 +0300

    Add project files.

---
+++ ConsoleApp1/Alimente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Alimente
+{
+    internal class Alimente
+    {
+        String alimente;
+        public Alimente()
+        {
+        }
+        public String setalimente(string alimente)
+        {
+            return this.alimente;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1.Alimente;
+int scor =0;
+int cantitateapa;
+
+Console.WriteLine("Wellness");
+
+Console.WriteLine("How much water do you drink in a day:");
+var cantitateapanevalidata = Console.ReadLine();
+while ((!(int.TryParse(cantitateapanevalidata, out cantitateapa))) && cantitateapa<=0 || cantitateapa>5 )
+{
+    Console.WriteLine("Introduce-ti o valoare intre 0 si 5:");
+    cantitateapanevalidata = Console.ReadLine();
+}
+
+scor += cantitateapa;
+Console.WriteLine(scor);
+
