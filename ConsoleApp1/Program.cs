99fafc9595e88b2c1b0146e30bfcf53970f11e57
Avrigeanu Sebastian <avrigeanusebastian@gmail.com>
2022-08-05 12:36:46 +0300

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -143,7 +143,8 @@ foreach (ref int index in sir)
 
 Console.WriteLine(suma);*/
 //279146358279
-string CNP = "5000314090045";
+Console.WriteLine("Introduce-ti CNP-ul");
+string CNP = Console.ReadLine();
 char[] chars = CNP.ToCharArray();
 int[] Aint = Array.ConvertAll(chars, c => (int)Char.GetNumericValue(c));
 int[] expresie= { 2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9};
@@ -151,10 +152,40 @@ int sum = 0;
 for(int i=0; i<expresie.Length; i++)
 {
     sum += Aint[i] * expresie[i];
+    switch (i)
+    {
+        case 0:
+            if (Aint[i] < 1 | Aint[i] > 8)
+            {
+                Console.WriteLine("Cifra sex incorecta");
+            }
+
+            break;
+        case 4:
+            int luna = Convert.ToInt32(string.Format("{0}{1}", Aint[i-1], Aint[i]));
+            if (luna < 1 | luna > 12) Console.WriteLine("Luna invalida");
+            break;
+        case 6:
+            int ziua = Convert.ToInt32(string.Format("{0}{1}", Aint[i - 1], Aint[i]));
+            if (ziua < 1 | ziua > 31) Console.WriteLine("Ziua invalida");
+            break;
+        case 8:
+            int codjudet = Convert.ToInt32(string.Format("{0}{1}", Aint[i - 1], Aint[i]));
+            if (codjudet < 1 | codjudet > 52) Console.WriteLine("Cod judet invalid");
+            break;
+    }
 }
 if(sum % 11 <10 && sum % 11 == Aint[12])
 {
-    Console.WriteLine("corect");
+    Console.WriteLine("CNP corect");
+}
+else if (sum % 11 == 10 && Aint[12] == 1)
+{
+    Console.WriteLine("CNP corect");
+}
+else
+{
+    Console.WriteLine("CNP incorect");
 }
 
 /*if (!(CNP.Length == 13)){
